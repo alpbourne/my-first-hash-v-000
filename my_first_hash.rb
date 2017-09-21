4b00317c425bbfc1ed448c057b32296f359620ed
alpbourne <alpbourne@gmail.com>
2017-09-21 14:59:39 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,8 +17,7 @@ end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-  #your code here!
+  return shipping_manifest = {"oil paintings" => 3}
 end
 
 def adding
