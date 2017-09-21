5644caa769b08d86ff7d1f235f46ec32caf522be
alpbourne <alpbourne@gmail.com>
2017-09-21 15:05:20 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,6 +22,6 @@ end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  hash["pearl necklace"] = 1
+  shipping_manifest["pearl necklace"] = 1
   return shipping_manifest
 end
